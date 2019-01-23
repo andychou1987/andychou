3aea37e33626fcf7c080b4f8d717ff06348ab7d9
Ali Hassan <ali.hassan1@sheridanc.ca.on>
2019-01-23 13:05:24 -0500

    Yet another commit

--- src/ca/sheridancollege/week2/softwarefundamentals/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/Card.java
@@ -16,6 +16,19 @@ public class Card {
     public Card (String s, int v){
         suit = s;
         value = v;
+        if (s.equals("Hearts") || s.equals("Spades") 
+                ||s.equals("Diamonds") || s.equals("Clubs")){
+         System.out.println("The suit parameter is valid");   
+        }
+        else
+            System.out.println("The suit value is invalid. "
+            + "Choose a valid option: Hearts, Spades, Clubs or Diamonds");
+        if(v>0 && v<14){
+            System.out.println("The card value is valid.");
+        }
+        else
+            System.out.println("Choose a valid card value 1-13 inclusives");
+        
     }
 
     /**
